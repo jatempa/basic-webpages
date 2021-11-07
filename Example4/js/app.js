8ea097596a1a8b730a294eebeba5746ca70a93cb
Jorge Atempa <atempaitm@gmail.com>
2021-11-06 17:30:41 -0700

    Example 4 - Show pokemon image and name

--- Example4/js/app.js
+++ Example4/js/app.js
@@ -1,9 +1,42 @@
+const BASE_URL = 'https://pokeapi.co'
+const BASE_API_URL = `${BASE_URL}/api/v2/pokemon`
+const BASE_SPRITES_URL = 'https://img.pokemondb.net/sprites/home/normal'
+
 const inputSearch = document.querySelector('input[type=text]')
 const btnSearch = document.querySelector('button#search')
+const pokemonResult = document.querySelector('article#result')
+
+const header = document.createElement('h2')
+const picture = document.createElement('img')
 
 const search = function() {
-  console.log("Hola Mundo")
+  
+  const pokemonName = inputSearch.value;
+
+  fetch(`${BASE_API_URL}/${pokemonName}`)
+    .then(response => response.json())
+    .then(result => {
+      const pokemon = result;
+      // console.log(pokemon)
+      const nameCapitalized = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)
+
+      header.innerText = nameCapitalized
+      picture.src = `${BASE_SPRITES_URL}/${pokemon.name}.png`
+
+      pokemonResult.appendChild(header)
+      pokemonResult.appendChild(picture)
+    })
+    .catch((e) => console.log('No se encontraron resultados'))
+
   inputSearch.value = ''
 }
 
 btnSearch.addEventListener('click', search)
+
+/*
+        <h2>
+          Ditto
+        </h2>
+        <img src="ditto.png" alt="sprite">
+
+*/
