d4ca8f89cfa0028150b736ca1d2c78a945975441
Jorge Atempa <atempaitm@gmail.com>
2022-05-02 19:56:53 -0700

    Create fake data for every topic

--- Example1/js/app.js
+++ Example1/js/app.js
@@ -1,18 +1,61 @@
-const main = document.querySelector('main')
+const main = document.querySelector("main");
 
-const mainGoal = document.createElement('h3')
-const mainGoalDescription = document.createElement('p')
-const syllabus = document.createElement('h3')
+const mainGoal = document.createElement("h3");
+const mainGoalDescription = document.createElement("p");
+const syllabus = document.createElement("h3");
+const content = document.createElement("section");
 
-mainGoal.innerText = 'Objetivo'
-mainGoal.className = 'subheading'
-main.appendChild(mainGoal)
+mainGoal.innerText = "Objetivo";
+mainGoal.className = "subheading";
+main.appendChild(mainGoal);
 
-mainGoalDescription.innerText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis assumenda enim aspernatur repudiandae ratione accusamus. Recusandae quibusdam quidem fugit, itaque doloribus distinctio possimus eius expedita eveniet unde dolores aut cupiditate.'
-mainGoalDescription.id = 'main-objective'
+mainGoalDescription.innerText =
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis assumenda enim aspernatur repudiandae ratione accusamus. Recusandae quibusdam quidem fugit, itaque doloribus distinctio possimus eius expedita eveniet unde dolores aut cupiditate.";
+mainGoalDescription.id = "main-objective";
 
-main.appendChild(mainGoalDescription)
+main.appendChild(mainGoalDescription);
 
-syllabus.innerText = 'Temario'
-syllabus.className = 'subheading'
-main.appendChild(syllabus)
+syllabus.innerText = "Temario";
+syllabus.className = "subheading";
+main.appendChild(syllabus);
+
+content.id = "content";
+
+const topicsList = [
+  {
+    title: "Unidad 1 - Introducci&oacute;n",
+    topics: ["Tema 1", "Tema 2", "Tema 3"],
+  },
+  {
+    title: "Unidad 2 - HTML y CSS",
+    topics: ["Tema 1", "Tema 2", "Tema 3"],
+  },
+  {
+    title: "Unidad 3 - CSS avanzado",
+    topics: ["Tema 1", "Tema 2", "Tema 3"],
+  },
+  {
+    title: "Unidad 4 - Control de versiones",
+    topics: ["Tema 1", "Tema 2", "Tema 3"],
+  },
+  {
+    title: "Unidad 5 - Pruebas y Despliegue",
+    topics: ["Tema 1", "Tema 2", "Tema 3"],
+  },
+];
+
+content.innerHTML = topicsList
+  .map((topic) => {
+    return `
+      <article>
+        <h3 class="topic-title">${topic.title}</h3>
+        <ul>
+          ${topic.topics.map((t) => `<li>${t}</li>`).join("")}
+        </ul>
+        </ul>
+      </article>
+  `;
+  })
+  .join("");
+
+main.appendChild(content);
